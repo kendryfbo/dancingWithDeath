6362870bce1c9bd0c65e32961de5bad27bcefedd
Kendry <kendryfbo@gmail.com>
2017-06-13 11:51:23 -0400

    Creation - Master Layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Appointment</title>
+
+		<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+
+		<link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+	</head>
+	<body>
+
+		@yield('content')
+
+		<script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
+		<script src="{{asset('js/tether.min.js')}}"></script>
+		<script src="{{asset('js/bootstrap.min.js')}}"></script>
+		<script src="{{asset('js/axios.min.js')}}"></script>
+		<script src="{{asset('js/vue.js')}}"></script>
+		<script src="{{asset('js/app.js')}}"></script>
+	</body>
+
+
+</html>
